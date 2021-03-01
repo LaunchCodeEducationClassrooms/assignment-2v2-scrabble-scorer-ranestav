cc964e8b635cee3fe8ab3918aa16487a0e7d44c8
ranestav <christianh82995@gmail.com>
2021-03-01 15:47:11 +0000

    fixing readline sync attempt

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -1,8 +1,6 @@
 // inspired by https://exercism.io/tracks/javascript/exercises/etl/solutions/91f99a3cca9548cebe5975d7ebca6a8
 const input = require('readline-sync');
 
-
-
 const oldPointStructure = {
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
   2: ['D', 'G'],
